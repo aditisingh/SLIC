9cb687f136c55eea2b9fc52740a5d246c0a36d13
Aditi Singh <787aditi@gmail.com>
2016-11-01 19:22:46 -0500

    Fixed weights

--- slic_mod.m
+++ slic_mod.m
@@ -17,32 +17,33 @@ labelled=zeros(img_ht,img_wd);
 
 S=floor(sqrt(N/K));    %size of superpixel
 
-%Setting parameters
+%Setting parameters 
+%Normalized weights
 
 lambda_1 =0;%weight of P4110_CO_
-lambda_2 =1;%weight of P4110_C1_: GAD67
-lambda_3 =1;%weight of P4110_C2_: Parvalbumin
-lambda_4 =1;%weight of P4110_C4_: APC
-lambda_5 =1;%weight of P4110_C5_: RECA1
-lambda_6 =1;%weight of P4110_C6_: S100
-lambda_7 =1;%weight of P4110_C7_: NeuN
-lambda_8 =1;%weight of P4110_C8_: IBA1
-lambda_9 =1;%weight of P4110_C10: DAPI
+lambda_2 =1/637.5191;% b=255, g=000, r=000; weight of P4110_C1_: GAD67
+lambda_3 =1/1570.8;% b=255, g=000, r=000; weight of P4110_C2_: Parvalbumin
+lambda_4 =1/1055.5;% b=123, g=255, r=000; weight of P4110_C4_: APC
+lambda_5 =1/952.3659;% b=000, g=255, r=000; weight of P4110_C5_: RECA1
+lambda_6 =1/3271.8;% b=000, g=255, r=255; weight of P4110_C6_: S100
+lambda_7 =1/1804.6;%weight of P4110_C7_: NeuN
+lambda_8 =1/785.4874;%weight of P4110_C8_: IBA1
+lambda_9 =1/1114.8;% b=255, g=255, r=255; weight of P4110_C10: DAPI
 lambda_10=0;%weight of P4111_C0_
-lambda_11=1;%weight of P4111_C1_: TubulinBeta3
-lambda_12=1;%weight of P4111_C2_: MAP2
-lambda_13=1;%weight of P4111_C5_: PLP
-lambda_14=1;%weight of P4111_C8_: GFAP
+lambda_11=1/3874.8;%weight of P4111_C1_: TubulinBeta3
+lambda_12=1/1310.3;%weight of P4111_C2_: MAP2
+lambda_13=1/7455.7;%weight of P4111_C5_: PLP
+lambda_14=1/1843.4;%weight of P4111_C8_: GFAP
 lambda_15=0;%weight of P4112_C0_ 
-lambda_16=1;%weight of P4112_C5_: PCNA
-lambda_17=1;%weight of P4112_C6_: CC3
-lambda_18=1;%weight of P4112_C7_: NFH
+lambda_16=1/2907.1;%weight of P4112_C5_: PCNA
+lambda_17=1/1268.2;%weight of P4112_C6_: CC3
+lambda_18=1/5849.8;%weight of P4112_C7_: NFH
 lambda_19=0;%weight of P4113_C0_
-lambda_20=1;%weight of P4113_C4_: Claretinin
-lambda_21=1;%weight of P4113_C5_: SynaptoPhys
-lambda_22=1;%weight of P4113_C6_: GLAST
-lambda_23=1;%weight of P4113_C7_: MBP
-lambda_24=1;%weight of P4113_C8_: TomatoLectin
+lambda_20=1/1853.4;%weight of P4113_C4_: Claretinin
+lambda_21=1/11269;%weight of P4113_C5_: SynaptoPhys
+lambda_22=1/10606;%weight of P4113_C6_: GLAST
+lambda_23=1/17777;%weight of P4113_C7_: MBP
+lambda_24=1/4387.4;%weight of P4113_C8_: TomatoLectin
 
 m=10; %compactness control constant
 
